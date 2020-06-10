363e00bba74457bcf3cfbbe330350b22c472ccc6
Minh Pham <pdminh0410@gmail.com>
2020-06-10 23:32:25 +0700

    Refactoring to Kotlin - 4. Default arguments, named arguments

--- src/User.kt
+++ src/User.kt
@@ -1,1 +1,6 @@
-data class User(var firstName: String?, var lastName: String?)
+data class User(var firstName: String?, var lastName: String? = null)
+
+// usage
+val jane = User("Jane") // same as User("Jane", null)
+val joe = User("Joe", "Doe")
+val john = User(firstName = "John", lastName = "Doe")
